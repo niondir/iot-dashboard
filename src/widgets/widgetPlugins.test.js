c86c8210706e7344b895f42fd11f50c83447b429
Tobias Kaupat <tobias.kaupat@mgm-tp.com>
2016-06-14 12:01:35 +0700

    Fix Tests

--- src/widgets/widgetPlugins.test.js
+++ src/widgets/widgetPlugins.test.js
@@ -1,17 +1,17 @@
 import {assert} from 'chai'
-import WidgetPlugins from './widgetPlugins'
+import * as WidgetPlugins from './widgetPlugins'
 
 
 describe('Widget Plugins', function () {
     describe('#register() && #getWidget()', function () {
         it("It's possible to register and get back a plugin", function () {
-            WidgetPlugins.register({
+            WidgetPlugins.pluginRegistry.register({
                 TYPE_INFO: {
                     type: 'foo'
                 }
             });
 
-            let plugin = WidgetPlugins.getPlugin('foo');
+            let plugin = WidgetPlugins.pluginRegistry.getPlugin('foo');
 
             assert.isOk(plugin);
             assert.equal('foo', plugin.type);
