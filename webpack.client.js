9f6dd9c47baaf1e8815896bb2eb39786738d6feb
Tobias Kaupat <tobiall@niondir.de>
2016-07-20 01:11:36 +0200

    Prepare npm publishing

--- webpack.client.js
+++ webpack.client.js
@@ -10,13 +10,13 @@ webpackConfig.entry = {
         "react", "react-dom", "react-grid-layout", "react-grid-layout/css/styles.css",
         "redux", "react-redux", "redux-logger", "redux-thunk", "redux-form",
         "semantic-ui-css/semantic", "semantic-ui-css/semantic.css", "jquery", "c3css", "c3", "d3",
-        "form-serialize", "lodash", "loadjs", "urijs"
+        "form-serialize", "lodash", "loadjs", "urijs", "es6-promise"
     ]
 };
 
 webpackConfig.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({names: ["vendor"], filename: "vendor.bundle.js", chunks: ["app"]}),
+    new webpack.optimize.CommonsChunkPlugin({names: ["vendor"], filename: "vendor.bundle.js", minChunks: Infinity, chunks: ["app", "vendor"]}),
     new webpack.PrefetchPlugin(webpackConfig.paths.node_modules, 'semantic-ui-css/semantic.css'),
     new webpack.PrefetchPlugin(webpackConfig.paths.node_modules, 'react/lib/ReactDOM.js'),
     new webpack.PrefetchPlugin(webpackConfig.paths.node_modules, 'react-grid-layout/build/ReactGridLayout.js'),
